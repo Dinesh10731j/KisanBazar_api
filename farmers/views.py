9ea89f8d9cb1930a139b3a70190fd18adccf4c71
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-04-04 18:55:06 +0545

    Add farmer detail retrieval endpoint and update URL routing

--- farmers/views.py
+++ farmers/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
+def get_farmer(request,id):
+    return JsonResponse({"message": "Farmer details","status":200,"id":id})
--- kisanbazar_api/urls.py
+++ kisanbazar_api/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('users.urls'))
-    
+    path('api/v1/',include('users.urls')),
+    path('api/v1/farmer/',include('farmers.urls'))
 ]
