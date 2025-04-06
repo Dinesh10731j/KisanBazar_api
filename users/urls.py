3da9ce7a525a9dc6e570145518ac457433061825
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-04-06 19:39:05 +0545

    Add JWT authentication and user login functionality

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('users/register', views.register_user),
+    path('users/login', views.login_user),
     path('users/list',views.list_user)
     
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 import json
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.hashers import make_password
 
 @csrf_exempt
 def register_user(request):
@@ -12,22 +14,43 @@ def register_user(request):
             user = User(
                 username=data.get('username'),
                 email=data.get('email'),
-                password=data.get('password')
+                password=make_password(data.get('password')),
             )
             user.save()
-            return JsonResponse({'message': 'User registered successfully!'}, status=201)
+            refresh = RefreshToken.for_user(user)
+            return JsonResponse({
+                'message': 'User registered successfully!',
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh)
+            }, status=201)
         except ValidationError as e:
             return JsonResponse({'error': str(e)}, status=400)
 
-from django.http import JsonResponse
-from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
+
+@csrf_exempt        
+def login_user(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            user = User.objects.get(email=data.get('email'))
+            if user.check_password(data.get('password')):
+                refresh = RefreshToken.for_user(user)
+                return JsonResponse({
+                    'message': 'Login successful!',
+                    'access_token': str(refresh.access_token),
+                    'refresh_token': str(refresh)
+                }, status=200)
+            else:
+                return JsonResponse({'error': 'Invalid credentials'}, status=401)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+
 
 def list_user(request):
     if request.method == 'GET':
         try:
             users = User.objects.all()
-            user_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+            user_list = [{'id': user.id, 'username': user.username, 'email': user.email,'password':user.password} for user in users]
             return JsonResponse({'users': user_list}, status=200)
         except ValidationError as e:
             return JsonResponse({'error': str(e)}, status=400)
