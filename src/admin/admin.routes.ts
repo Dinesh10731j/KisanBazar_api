f0c6591344f903ce6ff30e8082b9284d82427590
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-05-13 18:13:58 +0545

    Add admin settings functionality with validation and password hashing

--- src/admin/admin.routes.ts
+++ src/admin/admin.routes.ts
@@ -6,7 +6,11 @@ import {
   deleteUser,
   changeUserRole,
   Orders,
+  adminSetting,
 } from "./admin.controller";
+import validate from "../middleware/validate";
+import { profileValidator } from "../validator/validator";
+
 const adminRouter = express.Router();
 
 adminRouter.get("/dashboard", adminDashBoard);
@@ -15,4 +19,5 @@ adminRouter.get("/manage-users", manageUsers);
 adminRouter.delete("/:id", deleteUser);
 adminRouter.patch("/:id", changeUserRole);
 adminRouter.get("/orders", Orders);
+adminRouter.put("/:userId", profileValidator,validate,adminSetting);
 export default adminRouter;
